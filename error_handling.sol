4fee864b872f866f3faff6627deb59b220a81d22
Akb-25 <25bakshay2004@gmail.com>
2024-02-13 19:00:22 +0530

    integer input added

--- error_handling.sol
+++ error_handling.sol
@@ -4,17 +4,17 @@ pragma solidity ^0.8.7;
 contract ErrorHandling {
     string public process = "It is a positive integer";
 
-    function requireCheck(uint _x) public view returns (string memory) {
+    function requireCheck(int _x) public view returns (string memory) {
         require(_x > 0, "Input must be a positive number");
         return process;
     }
 
-    function assertCheck(uint _x) public view returns (string memory) {
+    function assertCheck(int _x) public view returns (string memory) {
         assert(_x > 0);
         return process;
     }
 
-    function revertCheck(uint _x) public view returns (string memory) {
+    function revertCheck(int _x) public view returns (string memory) {
         if (_x <= 0) {
             revert("Input should be positive");
         }
